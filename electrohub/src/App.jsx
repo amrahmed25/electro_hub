23821a1d1bd350c5d781faed28f16b7f45e5ba8e
amrahmed25 <aamr46589@gmail.com>
2026-07-08 00:36:29 +0300

    updated

--- electrohub/src/App.jsx
+++ electrohub/src/App.jsx
@@ -1,15 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Store from './pages/Store';
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Components from "./pages/Components";
+import Datasheets from "./pages/Datasheets";
+import LogicSimulator from "./pages/LogicSimulator";
+import ProjectGenerator from "./pages/ProjectGenerator";
+import ShoppingCart from "./pages/ShoppingCart";
+import UserDashboard from "./pages/UserDashboard";
+import Social from "./pages/Social";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/store" element={<Store />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/components" element={<Components />} />
+      <Route path="/datasheets" element={<Datasheets />} />
+      <Route path="/logic" element={<LogicSimulator />} />
+      <Route path="/project-generator" element={<ProjectGenerator />} />
+      <Route path="/shopping-cart" element={<ShoppingCart />} />
+      <Route path="/user-dashboard" element={<UserDashboard />} />
+      <Route path="/social" element={<Social />} />
+    </Routes>
   );
 }
 
